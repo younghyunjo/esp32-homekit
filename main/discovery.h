fceae3964230d4a6fe32f90b7ef323bd87e99c72
Younghyun Jo <younghyunjo99@gmail.com>
2017-10-25 19:08:35 +0900

    accessory dicovery feature added

---
+++ main/discovery.h
@@ -0,0 +1,16 @@
+#ifndef _DISCOVERY_H_
+#define _DISCOVERY_H_
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "hap.h"
+
+int discovery_init(const char* host, const int port, const char* model_name,
+        const uint32_t config_number, const enum hap_accessory_category category);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif //#ifndef _DISCOVERY_H_
