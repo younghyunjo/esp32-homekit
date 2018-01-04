835b157f0e774775b237c4821ea70ac2a663c770
Younghyun Jo <younghyunjo99@gmail.com>
2018-01-04 21:35:19 +0900

    use mbedtls srp library

--- main/pairing.c
+++ main/pairing.c
@@ -2,13 +2,13 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
-#include <freertos/FreeRTOS.h>
+
 
 #include "hap.h"
 #include "tlv.h"
-#include "srp.h"
+#include "hap_srp.h"
 
-//#define DEBUG
+#define DEBUG
 
 static const char* header = 
     "HTTP/1.1 200 OK\r\n"
@@ -20,20 +20,70 @@ static const char* header =
 static int _pairing_setup_m4(char* req_body, int req_body_length, 
         char** res_header, char** res_body, int* body_len)
 {
+    int error = 0;
+    struct tlv* A = tlv_decode((uint8_t*)req_body, req_body_length, 
+            HAP_TLV_TYPE_PUBLICKEY);
+    hsrp_verify_A(&A->value, A->length);
+    tlv_decoded_item_free(A);
+
     struct tlv* proof = tlv_decode((uint8_t*)req_body, req_body_length, 
             HAP_TLV_TYPE_PROOF);
+    int authenticated = hsrp_verify_session(&proof->value);
+    tlv_decoded_item_free(proof);
 
-    struct tlv* public_key = tlv_decode((uint8_t*)req_body, req_body_length, 
-            HAP_TLV_TYPE_PUBLICKEY);
+    if (authenticated == 0) {
+        printf("AUTH FAILED\n");
+        return 500;
+    }
 
-    printf("pubkey len:%d proof len:%d\n", public_key->length, proof->length);
+    uint8_t* accessory_proof = hsrp_hamk();
+    int res_body_length = 30;
 
-    if (public_key)
-        tlv_decoded_item_free(public_key);
+    uint8_t state[] = {0x04};
+    res_body_length += tlv_encode_length(sizeof(state));
+    res_body_length += tlv_encode_length(SRP_PROOF_LENGTH);
 
-    if (proof)
-        tlv_decoded_item_free(proof);
+    (*res_body) = malloc(res_body_length);
+    if (*res_body == NULL) {
+        printf("malloc failed. size:%d\n", res_body_length);
+        return 500;
+    }
+    memset(*res_body, 0, res_body_length);
 
+    uint8_t* tlv_encode_ptr = (uint8_t*)(*res_body);
+
+    tlv_encode_ptr += sprintf((char*)tlv_encode_ptr, "%lX\r\n", (unsigned long)tlv_encode_length(sizeof(state)));
+    tlv_encode_ptr += tlv_encode(HAP_TLV_TYPE_STATE, sizeof(state), state, tlv_encode_ptr);
+    tlv_encode_ptr += sprintf((char*)tlv_encode_ptr, "\r\n");
+
+    if (error) {
+        uint8_t error[] = {0x2};
+
+        tlv_encode_ptr += sprintf((char*)tlv_encode_ptr, "%lX\r\n", (unsigned long)tlv_encode_length(sizeof(error)));
+        tlv_encode_ptr += tlv_encode(HAP_TLV_TYPE_ERROR, sizeof(error), error, tlv_encode_ptr);
+        tlv_encode_ptr += sprintf((char*)tlv_encode_ptr, "\r\n");
+    }
+    else {
+        tlv_encode_ptr += sprintf((char*)tlv_encode_ptr, "%lX\r\n", (unsigned long)tlv_encode_length(SRP_PROOF_LENGTH));
+        tlv_encode_ptr += tlv_encode(HAP_TLV_TYPE_PROOF, SRP_PROOF_LENGTH, accessory_proof, tlv_encode_ptr);
+        tlv_encode_ptr += sprintf((char*)tlv_encode_ptr, "\r\n");
+    }
+
+    tlv_encode_ptr += sprintf((char*)tlv_encode_ptr, "0\r\n\r\n");
+
+    *body_len = (int)((char*)tlv_encode_ptr - *res_body);
+
+#if 1
+    printf("[PAIRING] m4 res start\n");
+    for (int i=0; i<*body_len; i++) {
+        if (i != 0 && (i % 0x10) == 0)
+            printf("\n");
+        printf("0x%02X ", (*res_body)[i]);
+    }
+    printf("\n[PAIRING res end(%d)\n", *body_len);
+#endif
+
+    *res_header = (char*)header;
     return 200;
 }
 
@@ -44,8 +94,8 @@ static int _pairing_setup_m2(char* req_body, int req_body_length,
 
     uint8_t state[] = {0x02};
     res_body_length += tlv_encode_length(sizeof(state));
-    res_body_length += tlv_encode_length(SRP_PUBLICH_KEY_LENGTH);
-    res_body_length += tlv_encode_length(SRP_SALT_LENGTH);
+    res_body_length += tlv_encode_length(HSRP_B_LENGTH);
+    res_body_length += tlv_encode_length(HSRP_SALT_LENGTH);
 
 #ifdef DEBUG
     printf("[PAIRING]m2 response body length:%d\n", res_body_length);
@@ -58,20 +108,17 @@ static int _pairing_setup_m2(char* req_body, int req_body_length,
     }
     memset(*res_body, 0, res_body_length);
 
-    static uint8_t publick_key[SRP_PUBLICH_KEY_LENGTH] = {0,};
-    srp_publich_key_get(publick_key);
-
-    uint8_t salt[SRP_SALT_LENGTH] = {0,};
-    srp_salt_get(salt);
+    uint8_t* salt = hsrp_salt();
+    uint8_t* B = hsrp_B();
 
     uint8_t* tlv_encode_ptr = (uint8_t*)(*res_body);
 
-    tlv_encode_ptr += sprintf((char*)tlv_encode_ptr, "%lX\r\n", (unsigned long)tlv_encode_length(SRP_SALT_LENGTH));
-    tlv_encode_ptr += tlv_encode(HAP_TLV_TYPE_SALT, SRP_SALT_LENGTH, salt, tlv_encode_ptr);
+    tlv_encode_ptr += sprintf((char*)tlv_encode_ptr, "%lX\r\n", (unsigned long)tlv_encode_length(HSRP_SALT_LENGTH));
+    tlv_encode_ptr += tlv_encode(HAP_TLV_TYPE_SALT, HSRP_SALT_LENGTH, salt, tlv_encode_ptr);
     tlv_encode_ptr += sprintf((char*)tlv_encode_ptr, "\r\n");
 
-    tlv_encode_ptr += sprintf((char*)tlv_encode_ptr, "%lX\r\n", (unsigned long)tlv_encode_length(SRP_PUBLICH_KEY_LENGTH));
-    tlv_encode_ptr += tlv_encode(HAP_TLV_TYPE_PUBLICKEY, SRP_PUBLICH_KEY_LENGTH, publick_key, tlv_encode_ptr);
+    tlv_encode_ptr += sprintf((char*)tlv_encode_ptr, "%lX\r\n", (unsigned long)tlv_encode_length(HSRP_B_LENGTH));
+    tlv_encode_ptr += tlv_encode(HAP_TLV_TYPE_PUBLICKEY, HSRP_B_LENGTH, B, tlv_encode_ptr);
     tlv_encode_ptr += sprintf((char*)tlv_encode_ptr, "\r\n");
 
 
@@ -84,12 +131,13 @@ static int _pairing_setup_m2(char* req_body, int req_body_length,
     *body_len = (int)((char*)tlv_encode_ptr - *res_body);
 
 #ifdef DEBUG
+    printf("[PAIRING] res start\n");
     for (int i=0; i<*body_len; i++) {
-        if (i != 0 && (i % 0x16) == 0)
+        if (i != 0 && (i % 0x10) == 0)
             printf("\n");
         printf("0x%02X ", (*res_body)[i]);
     }
-    printf("\n");
+    printf("res end\n");
 #endif
 
     *res_header = (char*)header;
@@ -116,11 +164,11 @@ int pairing_setup(char* req_body, int req_body_length,
     *res_body = NULL;
     *body_len = 0;
 
-#ifdef DEBUG
     printf("[PAIRING]body len:%d\n", req_body_length);
+#ifdef DEBUG
     printf("[PAIRING]body start\n");
     for (int i=0; i<req_body_length; i++) {
-        if (i != 0 && (i % 0x16) == 0)
+        if (i != 0 && (i % 0x10) == 0)
             printf("\n");
         printf("0x%02X ", req_body[i]);
     }
@@ -128,9 +176,9 @@ int pairing_setup(char* req_body, int req_body_length,
 #endif
 
     uint8_t state = _state_get(req_body, req_body_length);
-#ifdef DEBUG
+//#ifdef DEBUG
     printf("[PAIRING] state:%d\n", state);
-#endif
+//#endif
 
     switch (state) {
     case 0x01:
