4951a2cd6ac7f14cc5fc13a6d8241b83d689f04a
Younghyun Jo <younghyunjo99@gmail.com>
2018-01-07 15:53:18 +0900

    ed25519 module added

---
+++ main/ed25519.c
@@ -0,0 +1,69 @@
+#include <stdint.h>
+#include <esp_log.h>
+
+#include <wolfssl/wolfcrypt/ed25519.h>
+#include <wolfssl/wolfcrypt/curve25519.h>
+
+#include "ed25519.h"
+
+#define TAG "ED25519"
+
+int ed25519_key_generate(uint8_t public_key[], uint8_t private_key[])
+{
+    ed25519_key ed25519_key;
+    int err = wc_ed25519_init(&ed25519_key);
+    if (err < 0) {
+        ESP_LOGE(TAG, "wc_ed25519_init failed. err:%d\n", err);
+        return -1;
+    }
+
+    WC_RNG rng;
+    err = wc_ed25519_make_key(&rng, ED25519_KEY_LENGTH, &ed25519_key);
+    if (err < 0) {
+        ESP_LOGE(TAG, "wc_ed25519_make_key failed. err:%d\n", err);
+        return -1;
+    }
+
+    int key_length = ED25519_KEY_LENGTH;
+    wc_ed25519_export_public(&ed25519_key, (byte*)public_key, (word32*)&key_length);
+
+    key_length = ED25519_KEY_LENGTH;
+    wc_ed25519_export_private(&ed25519_key, (byte*)private_key, (word32*)&key_length);
+
+    return 0;
+}
+
+int ed25519_verify(uint8_t* public_key, int key_len, uint8_t* signature, int signature_len, uint8_t* msg, int msg_len)
+{
+    ed25519_key ed25519_key;
+    int err = wc_ed25519_init(&ed25519_key);
+    if (err < 0) {
+        ESP_LOGE(TAG, "wc_ed25519_init. err:%d\n", err);
+        return err;
+    }
+
+    err = wc_ed25519_import_public(public_key, key_len, &ed25519_key);
+    if (err < 0) {
+        ESP_LOGE(TAG, "wc_ed25519_import_public. err:%d\n", err);
+        return err;
+    }
+
+    int verified = 0;
+    err = wc_ed25519_verify_msg(signature, signature_len, msg, msg_len, 
+            &verified, &ed25519_key);
+    if (err < 0) {
+        ESP_LOGE(TAG, "wc_ed25519_verify_msg. err:%d\n", err);
+        return err;
+    }
+
+    return verified;
+}
+
+int ed25519_sign(uint8_t public_key[], uint8_t private_key[], uint8_t* in, int in_len, uint8_t* signatured, int* signatured_len)
+{
+    ed25519_key ed25519_key;
+    wc_ed25519_init(&ed25519_key);
+    wc_ed25519_import_private_key(private_key, ED25519_KEY_LENGTH, public_key, ED25519_KEY_LENGTH, &ed25519_key);
+    wc_ed25519_sign_msg(in, in_len, signatured, (word32*)signatured_len, &ed25519_key);
+    return 0;
+}
---
+++ main/ed25519.h
@@ -0,0 +1,22 @@
+#ifndef _ED25519_H_
+#define _ED25519_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+
+#define ED25519_KEY_LENGTH  32
+
+int ed25519_key_generate(uint8_t public_key[], uint8_t private_key[]);
+int ed25519_verify(uint8_t* public_key, int key_len, 
+        uint8_t* signature, int signature_len, uint8_t* msg, int msg_len);
+
+int ed25519_sign(uint8_t public_key[], uint8_t private_key[], uint8_t* in, int in_len, uint8_t* signatured, int* signated_len);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif //#ifndef _ED25519_H_
