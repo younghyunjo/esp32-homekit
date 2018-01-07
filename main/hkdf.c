f285fb9e060aaec7efa65ce7f7ad99fee413d468
Younghyun Jo <younghyunjo99@gmail.com>
2018-01-07 15:54:04 +0900

    hkdf module added

---
+++ main/hkdf.c
@@ -0,0 +1,51 @@
+#include <esp_log.h>
+#include <wolfssl/wolfcrypt/hmac.h>
+
+#include "hkdf.h"
+
+#define TAG "HKDF"
+
+struct hkdf_salt_info {
+    char* salt;
+    char* info;
+};
+
+static struct hkdf_salt_info _hkdf_salt_info[] = {
+    {   /* HKDF_KEY_TYPE_PAIR_SETUP_ENCRYPT */
+        .salt = "Pair-Setup-Encrypt-Salt",
+        .info = "Pair-Setup-Encrypt-Info",
+    },
+    {   /* HKDF_KEY_TYPE_PAIR_SETUP_CONTROLLER */
+        .salt = "Pair-Setup-Controller-Sign-Salt",
+        .info = "Pair-Setup-Controller-Sign-Info",
+    },
+    {   /* HKDF_KEY_TYPE_PAIR_SETUP_ACCESSORY */
+        .salt = "Pair-Setup-Accessory-Sign-Salt",
+        .info = "Pair-Setup-Accessory-Sign-Info",
+    }
+};
+
+static struct hkdf_salt_info* _salt_info_get(enum hkdf_key_type type)
+{
+    return &_hkdf_salt_info[type];
+}
+
+int hkdf_key_get(enum hkdf_key_type type, uint8_t* inkey, int inkey_len, uint8_t* outkey)
+{
+    uint8_t key[CHACHA20_POLY1305_AEAD_KEYSIZE];
+    struct hkdf_salt_info* salt_info = _salt_info_get(type);
+
+    int err = wc_HKDF(SHA512, inkey, inkey_len, 
+            (uint8_t*)salt_info->salt, strlen(salt_info->salt), 
+            (uint8_t*)salt_info->info, strlen(salt_info->info),
+            key, CHACHA20_POLY1305_AEAD_KEYSIZE);
+
+    if (err < 0) {
+        ESP_LOGE(TAG, "wc_HKDF failed. %d\n", err);
+        return err;
+    }
+
+    memcpy(outkey, key, CHACHA20_POLY1305_AEAD_KEYSIZE);
+    return 0;
+}
+
---
+++ main/hkdf.h
@@ -0,0 +1,27 @@
+#ifndef _HKDF_H_
+#define _HKDF_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+#include <wolfssl/wolfcrypt/chacha20_poly1305.h>
+
+#define HKDF_KEY_LEN      CHACHA20_POLY1305_AEAD_KEYSIZE
+
+enum hkdf_key_type {
+    HKDF_KEY_TYPE_PAIR_SETUP_ENCRYPT,
+    HKDF_KEY_TYPE_PAIR_SETUP_CONTROLLER,
+    HKDF_KEY_TYPE_PAIR_SETUP_ACCESSORY,
+    HKDF_KEY_TYPE_LENGTH,
+};
+
+int hkdf_key_get(enum hkdf_key_type type, uint8_t* inkey, int inkey_len, uint8_t* outkey);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif //#ifndef _HKDF_H_
+
