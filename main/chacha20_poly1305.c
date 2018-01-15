dae42ac0ee0cccce218b374ad3d91185aff68c55
Younghyun Jo <younghyunjo99@gmail.com>
2018-01-15 22:07:48 +0900

    eror handling

--- main/chacha20_poly1305.c
+++ main/chacha20_poly1305.c
@@ -4,24 +4,27 @@
 #include <esp_log.h>
 #include <wolfssl/wolfcrypt/chacha20_poly1305.h>
 
+#include "chacha20_poly1305.h"
+
 #define TAG "CHACHA20_POLY1305"
 
+#define NONCE_LENGTH    12
 
-int chacha20_poly1305_decrypt(uint8_t* key, uint8_t* encrypted, int encrypted_len, uint8_t* decrypted)
+static uint8_t nonce[][NONCE_LENGTH] = {
+    {0, 0, 0, 0, 'P', 'S', '-', 'M', 's', 'g', '0', '5'},
+    {0, 0, 0, 0, 'P', 'S', '-', 'M', 's', 'g', '0', '6'},
+    {0, 0, 0, 0, 'P', 'V', '-', 'M', 's', 'g', '0', '2'},
+    {0, 0, 0, 0, 'P', 'V', '-', 'M', 's', 'g', '0', '3'},
+};
+
+static uint8_t* _type_to_nonce(enum chacha20_poly1305_type type) {
+    return nonce[type];
+}
+
+int chacha20_poly1305_decrypt(enum chacha20_poly1305_type type, uint8_t* key, 
+        uint8_t* encrypted, int encrypted_len, uint8_t* decrypted)
 {
-    uint8_t nonce[]= "0000PS-Msg05";
-    nonce[0]=0;
-    nonce[1]=0;
-    nonce[2]=0;
-    nonce[3]=0;
-
-    {
-        printf("%s\n", __func__);
-        printf("encrypted:%p\n", encrypted);
-        printf("encrypted_len:%d\n", encrypted_len);
-        printf("decrypted:%p\n", decrypted);
-    }
-
+    uint8_t* nonce = _type_to_nonce(type);
     uint8_t* cipher_text = encrypted;
     int cipher_text_len = encrypted_len - CHACHA20_POLY1305_AEAD_AUTHTAG_SIZE;
     uint8_t* auth_tag = encrypted + cipher_text_len;
@@ -32,19 +35,18 @@ int chacha20_poly1305_decrypt(uint8_t* key, uint8_t* encrypted, int encrypted_le
         return -1;
     }
 
-    printf("end\n");
-
     return 0;
 }
 
-int chacha20_poly1305_encrypt(uint8_t* key, uint8_t* plain_text, int plain_text_length, uint8_t* encrypted, uint8_t* auth_tag)
+int chacha20_poly1305_encrypt(enum chacha20_poly1305_type type, uint8_t* key, 
+        uint8_t* plain_text, int plain_text_length, 
+        uint8_t* encrypted, uint8_t* auth_tag)
 {
-    uint8_t nonce[]= "0000PS-Msg06";
-    nonce[0]=0;
-    nonce[1]=0;
-    nonce[2]=0;
-    nonce[3]=0;
-
-    wc_ChaCha20Poly1305_Encrypt(key, nonce, NULL, 0, plain_text, plain_text_length, encrypted, auth_tag);
+    uint8_t* nonce = _type_to_nonce(type);
+    int err = wc_ChaCha20Poly1305_Encrypt(key, nonce, NULL, 0, plain_text, plain_text_length, encrypted, auth_tag);
+    if (err < 0) {
+        ESP_LOGE(TAG, "wc_ChaCha20Poly1305_Encrypt failed. err:%d\n", err);
+        return -1;
+    }
     return 0;
 }
--- main/chacha20_poly1305.h
+++ main/chacha20_poly1305.h
@@ -5,9 +5,20 @@
 extern "C" {
 #endif
 
-int chacha20_poly1305_decrypt(uint8_t* key, 
+#define CHACHA20_POLY1305_AUTH_TAG_LENGTH   16
+
+enum chacha20_poly1305_type {
+    CHACHA20_POLY1305_TYPE_PS05,
+    CHACHA20_POLY1305_TYPE_PS06,
+    CHACHA20_POLY1305_TYPE_PV02,
+    CHACHA20_POLY1305_TYPE_PV03,
+};
+
+int chacha20_poly1305_decrypt(enum chacha20_poly1305_type type, uint8_t* key, 
         uint8_t* encrypted, int encrypted_len, uint8_t* decrypted);
-int chacha20_poly1305_encrypt(uint8_t* key, uint8_t* plain_text, int plain_text_length, uint8_t* encrypted, uint8_t* auth_tag);
+int chacha20_poly1305_encrypt(enum chacha20_poly1305_type type, uint8_t* key, 
+        uint8_t* plain_text, int plain_text_length, 
+        uint8_t* encrypted, uint8_t* auth_tag);
 
 #ifdef __cplusplus
 }
