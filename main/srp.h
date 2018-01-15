c4746c5ad4c9b5408c9202ee6cc0eb6e72144532
Younghyun Jo <younghyunjo99@gmail.com>
2018-01-15 22:05:20 +0900

    srp refactored to OO

--- main/srp.h
+++ main/srp.h
@@ -8,39 +8,22 @@ extern "C" {
 #include <stdbool.h>
 #include <stdint.h>
 
-#define SRP_SALT_LENGTH 16
-#define SRP_PUBLIC_KEY_LENGTH  384
-#define SRP_PROOF_LENGTH    64
+#define SRP_SALT_LENGTH         16
+#define SRP_PUBLIC_KEY_LENGTH   384
+#define SRP_PROOF_LENGTH        64
 #define SRP_SESSION_KEY_LENGTH  64
 
-int srp_init(const char* setup_code);
+int srp_client_proof_verify(void* instance, uint8_t* proof);
+int srp_host_proof_get(void* instance, uint8_t proof[]);
 
-/*
- * Make salt
- */
-uint8_t* srp_salt(void);
+int srp_client_key_set(void* instance, uint8_t* client_public_key);
+int srp_host_key_get(void* instance, uint8_t host_public_key[]);
 
-/*
- * Make ESP32 public key. Length is 384
- */
-uint8_t* srp_B(void);
+int srp_host_session_key(void* instance, uint8_t session_key[]);
+int srp_salt(void* instance, uint8_t salt[]);
 
-/*
- * Set iOS Public key
- */
-bool srp_A_set(uint8_t* A);
-
-/*
- * Verify iOS Device proof
- */
-bool srp_verify(uint8_t* proof);
-
-/*
- * Return ESP32 proof for iOS Device. Length is 64
- */
-uint8_t* srp_response(void);
-
-uint8_t* srp_session_key(void);
+void* srp_init(const char* setup_code);
+void srp_cleanup(void* instance);
 
 #ifdef __cplusplus
 }
