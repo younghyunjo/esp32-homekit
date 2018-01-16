5316090c93a8762b22a59fb4b839838a3604e69a
Younghyun Jo <younghyunjo99@gmail.com>
2018-01-16 22:10:03 +0900

    cpp compatible added

--- main/nvs.h
+++ main/nvs.h
@@ -1,8 +1,16 @@
 #ifndef _NVS_H_
 #define _NVS_H_
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 int nvs_get(char* key, uint8_t* value, int len);
 int nvs_set(char* key, uint8_t* value, int len);
 int nvs_erase(char* key);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif //#ifndef _NVS_H_
